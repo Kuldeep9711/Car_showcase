bd2aa98709a9f1a8bf6155653e3603893265f337
Kuldeep Saini <k72125642@gmail.com>
2026-01-09 20:30:06 +0530

    add css in hero and navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
    }
 
   return (
-    <div className='hero'>
+    <div className=' hero mt-20'>
      <div className='flex-1 pt-36 padding-x'>
         <h1 className='hero__title'>
             Find, book, or rent a car — quickly and easily!
