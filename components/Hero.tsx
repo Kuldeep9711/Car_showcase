57f3343d623e71ae7b8d2855532b47f0bfec87fd
Kuldeep Saini <k72125642@gmail.com>
2026-01-09 19:43:32 +0530

    add custom image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+"use client"
+
+import Image from 'next/image'
 import CustomButton from './CustomButton'
 
 const Hero = () => {
+   const handleScroll = () => {
+
+   }
+
   return (
     <div className='hero'>
      <div className='flex-1 pt-36 padding-x'>
@@ -13,8 +19,25 @@ const Hero = () => {
             Streamline your car rental experience with our effortless booking process.
         </p>
 
-        <CustomButton />
+        <CustomButton 
+          title="Explore Cars"
+            containerStyles="bg-blue-600 text-white hover:bg-blue-700"
+        //   containerStyles="bg-primary-blue text-white rounded-full mt-10"
+           handleClick={handleScroll}
+        />
      </div>
+
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+             <Image
+               src="/hero.png"
+               alt="hero"
+               fill
+              className='object-contain'
+             />
+             <div className='hero__image-overlay' />
+        </div>
+      </div>
     </div>
   )
 }
