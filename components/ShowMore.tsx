7705844a3235f6b2117c61417dbb72b7a1718574
Kuldeep Saini <k72125642@gmail.com>
2026-01-24 23:46:39 +0530

    add limit

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -3,12 +3,16 @@
 import { ShowMoreProps } from "@/types"
 import { useRouter } from "next/navigation"
 import CustomButton from "./CustomButton";
+import { updateSearchParams } from "@/app/utils";
 
 
 const ShowMore = ({ pageNumber, isNext}: ShowMoreProps) => {
     const router = useRouter();
 
-   const handleNavigation = () => {}
+   const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit",)
+   }
 
   return (
     <div className="w-full flex-center gap-5 mt-10">
